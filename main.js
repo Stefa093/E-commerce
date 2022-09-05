ce71f5847fec55f53958ef64d7eaf9c685a66854
Stefa093 <estefaniasalazar093@gmail.com>
2022-09-05 14:59:21 -0500

    v3

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-import { navMenu } from "./js/menu.js";
-import { navLink } from "./js/menu.js";
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-tgl'),
+    closeMenu = document.getElementById('nav-close')
+
+toggleMenu.addEventListener('click', ()=> {
+    navMenu.classList.toggle('show')
+})
+
+closeMenu.addEventListener('click', ()=> {
+    navMenu.classList.remove('show')
+})
+
+// Clik Link
+
+const navLink = document.querySelectorAll('.nlink')
+
+function linkAction() { 
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 let darkToggle = document.querySelector('#darkToggle');
 
@@ -7,7 +26,7 @@ darkToggle.addEventListener('click', ()=> {
 document.body.classList.toggle('dark');
 })
 
-window.addEventListener("load", function (params) {
+window.addEventListener("load", function () {
     const loaded = this.document.querySelector("#load");
     setTimeout(() => {
         loaded.style.display = "none";
@@ -22,4 +41,3 @@ mixitup(".prod-cont", {
         duration: 300
     }
 }).filter("all")
-
